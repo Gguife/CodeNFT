25d62431720003e6686314d09985036d0449aa63
Gguife <gguife747@gmail.com>
2023-11-20 19:13:47 -0300

    feat: Finished resposive Hero section and your animation

--- src/components/hero/Index.tsx
+++ src/components/hero/Index.tsx
@@ -1,4 +1,5 @@
 import "./Style.css";
+import { motion } from "framer-motion";
 //Images
 import Logo from "../menu/Logo";
 import HeroNFT from "../../assets/nft-hero.png";
@@ -6,32 +7,47 @@ import HeroNFT from "../../assets/nft-hero.png";
 const Hero = () =>{
   return(
     <section id="hero">
-      <div className="contentHero">
+      <motion.div className="box contentHero"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 1,
+        delay: .3,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}>
         <h1>Descubrar e colete <span>obras de arte</span> digitais super raras</h1>
         <p>O maior mercado NFT. Criação digital autêntica e verdadeiramente única. Assinado e emitido pelo criador, possibilitado pela tecnologia blockchain</p>
         <button className="button">Explorar</button>
-      </div>
-        <div className="heroCard">
-          <div className="heroTop">
-            <div className="rate">
-              <p>TOP 1</p>
-            </div>
-            <img className="heroImg" src={HeroNFT} alt="" />
+      </motion.div>
+
+      <motion.div className="box heroCard"    
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 1,
+        delay: .5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}>
+        <div className="heroTop">
+          <div className="rate">
+            <p>TOP 1</p>
           </div>
-          <div className="cardBottom">
-            <div className="cardBottom-left">
-              <p>Apple Muu</p>
-              <span>By Steve Jobs</span>
-              <div className="cardBottom-left-price">
-                <Logo />
-                <p>18 ETH</p>
-              </div>
-            </div>
-            <div className="cardBottom-right">
-              <button className="button">Comprar</button>
+          <img className="heroImg" src={HeroNFT} alt="" />
+        </div>
+        <div className="cardBottom">
+          <div className="cardBottom-left">
+            <p>Apple Muu</p>
+            <span>By Steve Jobs</span>
+            <div className="cardBottom-left-price">
+              <Logo />
+              <p>18 ETH</p>
             </div>
           </div>
-      </div>
+          <div className="cardBottom-right">
+            <button className="button">Comprar</button>
+          </div>
+        </div>
+      </motion.div>
     </section>
   )
 }
