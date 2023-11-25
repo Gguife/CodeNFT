212a37f1ad75b6de4403c57556c81c0da0db3e7f
Gguife <gguife747@gmail.com>
2023-11-25 19:00:33 -0300

    Finished Number section and your slide responsive container

--- src/components/numbersCard/Index.tsx
+++ src/components/numbersCard/Index.tsx
@@ -1,72 +1,78 @@
 import "./Style.css";
 import { motion } from "framer-motion";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+
+const dashboardArray = [
+  {
+    title: "Coleções",
+    number: "300",
+  },
+  {
+    title: "Usuários",
+    number: "+100k",
+  },
+  {
+    title: "Negociações",
+    number: "+30k",
+  },
+  {
+    title: "Visualizações",
+    number: "+1 M",
+  },
+];
+
 const NumberCard = () =>{
   return(
-    <motion.div className="box numberCard"
-    initial={{ opacity: 0, scale: 0.5 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{
-      duration: 1,
-      delay: .6,
-      ease: [0, 0.71, 0.2, 1.01]
-    }}>
-      <div className="numberCard-container">
-        <motion.div className="box number-1"
+    <div className="numberCard">
+        <motion.div className="box numberCard-container"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
             duration: 1,
-            delay: .9,
+            delay: .7,
             ease: [0, 0.71, 0.2, 1.01]
         }}>
-          <p>300</p>
-          <span>Coleções</span>
+          {dashboardArray.map((i, k) =>(
+            <motion.div className="box numberCard-content"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            key={k}
+            transition={{
+              duration: 1,
+              delay: 0.9 + k * 0.3,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}>
+              <div className="number-desc">  
+                <p>{i.number}</p>
+                <span>{i.title}</span>
+              </div>
+              {k < 3 && <div className="line"></div>}
+            </motion.div> 
+          ))}
         </motion.div>
-
-        <div className="line"></div>
-        
-        <motion.div className="box number-2"
+        <motion.div className="box numberCard-slide"
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{
             duration: 1,
-            delay: 1.1,
+            delay: .7,
             ease: [0, 0.71, 0.2, 1.01]
         }}>
-          <p>+100K</p>
-          <span>Usuários</span>
-        </motion.div> 
-        
-        <div className="line"></div>
-        
-        <motion.div className="box number-3"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 1,
-            delay: 1.3,
-            ease: [0, 0.71, 0.2, 1.01]
-        }}>
-          <p>+30K</p>
-          <span>Negociações</span>
+          <Swiper spaceBetween={0} slidesPerView={1}>
+            {dashboardArray.map((i, k) => (
+              <div key={k}>
+                <SwiperSlide className="numberSlide-content">
+                  <p>{i.number}</p>
+                  <span>{i.title}</span>
+                </SwiperSlide>
+              </div>
+            ))}
+          </Swiper>
         </motion.div>
-        
-        <div className="line"></div>
-        
-        <motion.div className="box number-4"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 1,
-            delay: 1.5,
-            ease: [0, 0.71, 0.2, 1.01]
-        }}>
-          <p>+1M</p>
-          <span>Vizualizações</span>
-        </motion.div>
-      </div>
-    </motion.div>
+    </div>
   )
 }
 
